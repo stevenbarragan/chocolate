e771be1c9f1f4c2a4d91826f302308ec7f844a17
Steven Barragan <me@steven.mx>
2014-01-27 01:24:29 -0600

    Report user login status

--- js/app.js
+++ js/app.js
@@ -3,15 +3,21 @@ Message = new Meteor.Collection('messages')
 Message.allow({
   insert: function(){
     return true
+  },
+  update: function(){
+    return true
   }
 })
 
-if (Meteor.isServer){
-  Meteor.publish("directory", function () {
-    return Meteor.users.find({}, {fields: {emails: 1, profile: 1}});
-  });
-}
-
 if (Meteor.isClient) {
   Meteor.subscribe("directory");
+
+  Meteor.autorun(function(){
+    if (Meteor.userId()){
+      Session.set('userId', Meteor.userId())
+      Meteor.users.update({_id: Meteor.userId()}, {$set: {status: 'logged'}})
+    }else{
+      Meteor.users.update({_id: Session.get('userId')}, {$set: {status: 'unlogged'}})
+    }
+  })
 }
